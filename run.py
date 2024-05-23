200b1a0ab6977e571c199f5ec63ae5c691379cdf
gstinoco <gstinoco@gmail.com>
2024-05-23 11:00:42 -0600

    Results computed for Clouds of points.

--- run.py
+++ run.py
@@ -29,6 +29,9 @@ for file in files:
             regions[region] = {}
         regions[region][suffix] = file
 
+# Should I save the results?
+Save = True
+
 # Load data files for each region.
 counter = 1
 for region, files in regions.items():
@@ -42,11 +45,8 @@ for region, files in regions.items():
         p  = np.genfromtxt(p_file_path, delimiter=',', skip_header = 0)
         tt = np.genfromtxt(tt_file_path, delimiter=',', skip_header = 0)
 
-
-
-
-        # Poisson Equation
-        print('Poisson Problem')
+        #########################  Poisson Equation #########################
+        print('\tPoisson Problem')
         ## Problem parameters
         phi = lambda x, y: 2*np.exp(2*x + y)
         f   = lambda x, y: 10*np.exp(2*x + y)
@@ -59,31 +59,30 @@ for region, files in regions.items():
 
         ##Compute the Error
         er = Errors.Cloud_Stationary(p, vec, u_ap, u_ex)
+        print('\t\tError: ', np.mean(er))
 
-        ## Save the Error and the solution
-        nom = os.path.join(results_path, 'Poisson', region, 'Error.txt')
-        with open(nom, 'w') as file:
-            file.write(str(np.mean(er)))
+        if Save:
+            ## Save the Error and the solution
+            nom = os.path.join(results_path, 'Poisson', region, 'Error.txt')
+            with open(nom, 'w') as file:
+                file.write(str(np.mean(er)))
 
-        nom = os.path.join(results_path, 'Poisson', region, 'Computed Solution.csv')
-        np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
-        
-        nom = os.path.join(results_path, 'Poisson', region, 'Theoretical Solution.csv')
-        np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
+            nom = os.path.join(results_path, 'Poisson', region, 'Computed Solution.csv')
+            np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
+            
+            nom = os.path.join(results_path, 'Poisson', region, 'Theoretical Solution.csv')
+            np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
 
         ## Plot the solution
         nom = os.path.join(results_path, 'Poisson', region, 'Solution.png')
-        Graph.Cloud_Stationary(p, tt, u_ap, u_ex, save = True, nom = nom)
-
-
-
-
-        # Heat Equation
-        print('Heat Problem')
+        Graph.Cloud_Stationary(p, tt, u_ap, u_ex, save = Save, nom = nom)
+        #################################################################
+         
+        ######################### Heat Equation #########################
+        print('\tHeat Problem')
         ## Problem Parameters
         v       = 0.2
         t       = 2000                                                                      # Number of time-steps.
-        Save    = False                                                                     # Should I save the results?
         f = lambda x, y, t, coef: np.exp(-2*np.pi**2*coef[0]*t)*np.cos(np.pi*x)*np.cos(np.pi*y)
 
         ## Operator
@@ -94,75 +93,35 @@ for region, files in regions.items():
 
         ##Compute the Error
         er = Errors.Cloud_Transient(p, vec, u_ap, u_ex)
+        print('\t\tError: ', np.mean(er))
 
-        ## Save the Error and the solution
-        nom = os.path.join(results_path, 'Heat', region, 'Error.txt')
-        with open(nom, 'w') as file:
-            file.write(str(np.mean(er)))
+        if Save:
+            ## Save the Error and the solution
+            nom = os.path.join(results_path, 'Heat', region, 'Error.txt')
+            with open(nom, 'w') as file:
+                file.write(str(np.mean(er)))
 
-        nom = os.path.join(results_path, 'Heat', region, 'Computed Solution.csv')
-        np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
-        
-        nom = os.path.join(results_path, 'Heat', region, 'Theoretical Solution.csv')
-        np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
+            nom = os.path.join(results_path, 'Heat', region, 'Computed Solution.csv')
+            np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
+            
+            nom = os.path.join(results_path, 'Heat', region, 'Theoretical Solution.csv')
+            np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
+
+            nom = os.path.join(results_path, 'Heat', region, 'Solution')
+            Graph.Cloud_Transient_Steps(p, tt, u_ap, u_ex, nom = nom)
 
         ## Plot the solution
         nom = os.path.join(results_path, 'Heat', region, 'Solution.mp4')
-        Graph.Cloud_Transient(p, tt, u_ap, u_ex, save = True, nom = nom)
+        Graph.Cloud_Transient(p, tt, u_ap, u_ex, save = Save, nom = nom)
+        #################################################################
 
-        nom = os.path.join(results_path, 'Heat', region, 'Solution')
-        Graph.Cloud_Transient_Steps(p, tt, u_ap, u_ex, nom = nom)
-
-
-
-
-        # Advection Equation
-        print('Advection Problem')
-        ## Problem Parameters
-        a    = 0.3
-        b    = 0.2
-        t    = 2000                                                                      # Number of time-steps.
-        Save = False                                                                     # Should I save the results?
-        f = lambda x, y, t, coef: 0.2*np.exp((-(x-.5-coef[0]*t)**2-(y-.5-coef[1]*t)**2)/.001)
-
-        ## Operator
-        L = np.vstack([[-a], [-b], [0], [0], [0], [0]])                                  # L = [D, E, A, B, C, F] from operator Lu = Au_xx + Bu_xy + Cu_yy + Du_x + Eu_y + F_u
-
-        ## Problem Solving
-        u_ap, u_ex, vec = TimeDerivative1(p, f, t, [a, b], operator = L, triangulation = False, tt = [], implicit = False, lam = 0.5)
-
-        ##Compute the Error
-        er = Errors.Cloud_Transient(p, vec, u_ap, u_ex)
-
-        ## Save the Error and the solution
-        nom = os.path.join(results_path, 'Advection', region, 'Error.txt')
-        with open(nom, 'w') as file:
-            file.write(str(np.mean(er)))
-
-        nom = os.path.join(results_path, 'Advection', region, 'Computed Solution.csv')
-        np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
-        
-        nom = os.path.join(results_path, 'Advection', region, 'Theoretical Solution.csv')
-        np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
-
-        ## Plot the solution
-        nom = os.path.join(results_path, 'Advection', region, 'Solution.mp4')
-        Graph.Cloud_Transient(p, tt, u_ap, u_ex, save = True, nom = nom)
-
-        nom = os.path.join(results_path, 'Advection', region, 'Solution')
-        Graph.Cloud_Transient_Steps(p, tt, u_ap, u_ex, nom = nom)
-
-
-
-
-        # Advection-Diffusion Equation
-        print('Advection-Diffusion Problem')
+        #########################  Advection-Diffusion Equation #########################
+        print('\tAdvection-Diffusion Problem')
         ## Problem Parameters
         v    = 0.1
         a    = 0.3
         b    = 0.2
         t    = 2000                                                                      # Number of time-steps.
-        Save = False                                                                     # Should I save the results?
         f = lambda x, y, t, coef: (1/(4*t+1))*np.exp(-(x-coef[1]*t-0.5)**2/(coef[0]*(4*t+1)) - (y-coef[2]*t-0.5)**2/(coef[0]*(4*t+1)))
 
         ## Operator
@@ -173,34 +132,33 @@ for region, files in regions.items():
 
         ##Compute the Error
         er = Errors.Cloud_Transient(p, vec, u_ap, u_ex)
+        print('\t\tError: ', np.mean(er))
 
-        ## Save the Error and the solution
-        nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Error.txt')
-        with open(nom, 'w') as file:
-            file.write(str(np.mean(er)))
+        if Save:
+            ## Save the Error and the solution
+            nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Error.txt')
+            with open(nom, 'w') as file:
+                file.write(str(np.mean(er)))
 
-        nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Computed Solution.csv')
-        np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
-        
-        nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Theoretical Solution.csv')
-        np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
+            nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Computed Solution.csv')
+            np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
+            
+            nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Theoretical Solution.csv')
+            np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
+
+            nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Solution')
+            Graph.Cloud_Transient_Steps(p, tt, u_ap, u_ex, nom = nom)
 
         ## Plot the solution
         nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Solution.mp4')
-        Graph.Cloud_Transient(p, tt, u_ap, u_ex, save = True, nom = nom)
+        Graph.Cloud_Transient(p, tt, u_ap, u_ex, save = Save, nom = nom)
+        #################################################################
 
-        nom = os.path.join(results_path, 'Advection-Diffusion', region, 'Solution')
-        Graph.Cloud_Transient_Steps(p, tt, u_ap, u_ex, nom = nom)
-
-
-
-
-        # Wave Equation
-        print('Wave Problem')
+        #########################  Wave Equation #########################
+        print('\tWave Problem')
         ## Problem Parameters
         c       = np.sqrt(1/2)                                                              # Wave coefficient.
         t       = 2000                                                                      # Number of time-steps.
-        Save    = False                                                                     # Should I save the results?
         f = lambda x, y, t, coef: np.cos(np.pi*t)*np.sin(np.pi*(x+y))                          # f = \cos{\pi t}\sin{\pi(x + y)}
         g = lambda x, y, t, coef: -np.sin(np.pi*t)*np.sin(np.pi*(x+y))                         # g = -\pi\sin{\pi t}\sin{\pi(x + y)}
 
@@ -212,21 +170,24 @@ for region, files in regions.items():
 
         ##Compute the Error
         er = Errors.Cloud_Transient(p, vec, u_ap, u_ex)
+        print('\t\tError: ', np.mean(er))
 
-        ## Save the Error and the solution
-        nom = os.path.join(results_path, 'Wave', region, 'Error.txt')
-        with open(nom, 'w') as file:
-            file.write(str(np.mean(er)))
+        if Save:
+            ## Save the Error and the solution
+            nom = os.path.join(results_path, 'Wave', region, 'Error.txt')
+            with open(nom, 'w') as file:
+                file.write(str(np.mean(er)))
 
-        nom = os.path.join(results_path, 'Wave', region, 'Computed Solution.csv')
-        np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
-        
-        nom = os.path.join(results_path, 'Wave', region, 'Theoretical Solution.csv')
-        np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
+            nom = os.path.join(results_path, 'Wave', region, 'Computed Solution.csv')
+            np.savetxt(nom, u_ap, delimiter=',', fmt='%d')
+            
+            nom = os.path.join(results_path, 'Wave', region, 'Theoretical Solution.csv')
+            np.savetxt(nom, u_ex, delimiter=',', fmt='%d')
+
+            nom = os.path.join(results_path, 'Wave', region, 'Solution')
+            Graph.Cloud_Transient_Steps(p, tt, u_ap, u_ex, nom = nom)
 
         ## Plot the solution
         nom = os.path.join(results_path, 'Wave', region, 'Solution.mp4')
-        Graph.Cloud_Transient(p, tt, u_ap, u_ex, save = True, nom = nom)
-
-        nom = os.path.join(results_path, 'Wave', region, 'Solution')
-        Graph.Cloud_Transient_Steps(p, tt, u_ap, u_ex, nom = nom)
+        Graph.Cloud_Transient(p, tt, u_ap, u_ex, save = Save, nom = nom)
+        #################################################################
